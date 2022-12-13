1aac308b3c3adc5445737fe231676dcf2a4b610f
Rafael Siqueira <rafaelsiqueira.dev@gmail.com>
2022-12-13 17:22:06 -0300

    feat: add player movement and collision with wall

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,50 +1,42 @@
-extends Sprite
+extends Area2D
 
-onready var moveTo = $MoveTo
-onready var destinationPos: Vector2 = $MoveTo.global_position
+onready var ray = $RayCast2D
+onready var animPlayer = $AnimationPlayer
 
-export var moveSpeed: float = 500
+var tile_size = 32
+var animation_speed = 20
+var moving = false
 
-var isMoving: bool = false
-
-var canMoveTo = {
-  "ui_up": true,
-  "ui_down": true,
-  "ui_left": true,
-  "ui_right": true,
+var inputs = {
+  "ui_right": Vector2.RIGHT,
+  "ui_left": Vector2.LEFT,
+  "ui_up": Vector2.UP,
+  "ui_down": Vector2.DOWN
 }
 
-func _input(event: InputEvent) -> void:
-  if (isMoving):
+func _ready():
+  position = position.snapped(Vector2.ONE * tile_size)
+  position += Vector2.ONE * tile_size/2
+
+func _unhandled_input(event):
+  if moving:
     return
+  for dir in inputs.keys():
+    if event.is_action_pressed(dir):
+      move(dir)
 
-  var dirStr: String = ""
+func move(dir):
+  ray.cast_to = inputs[dir] * tile_size
+  ray.force_raycast_update()
 
-  if event.is_action_pressed("ui_up"):      dirStr = "ui_up"
-  elif event.is_action_pressed("ui_down"):  dirStr = "ui_down"
-  elif event.is_action_pressed("ui_left"):  dirStr = "ui_left"
-  elif event.is_action_pressed("ui_right"): dirStr = "ui_right"
+  if !ray.is_colliding():
+    # position += inputs[dir] * tile_size
+    var tween = get_tree().create_tween()
+    tween.tween_property(self, "position", position + inputs[dir] * tile_size, 1.0 / animation_speed).set_trans(Tween.TRANS_SINE)
+    moving = true
+    # animPlayer.play(dir)
+    tween.tween_callback(self, 'allowMovement')
+    moving = false
 
-  if (dirStr != ""):
-    movePlayer(dirStr)
-
-func _process(delta):
-  if (isMoving):
-    global_position = $MoveTo.global_position.move_toward(destinationPos, delta * moveSpeed)
-
-  if global_position == destinationPos:
-    isMoving = false
-
-func movePlayer(direction: String):
-  isMoving = true
-  destinationPos = global_position + setVectorOnInput(direction)
-
-
-# Helpers
-
-func setVectorOnInput(inputStr: String):
-  match inputStr:
-    "ui_up":    return Vector2(0, -32)
-    "ui_down":  return Vector2(0, 32)
-    "ui_left":  return Vector2(-32, 0)
-    "ui_right": return Vector2(32, 0)
+func allowMovement():
+  moving = false
